c5f7382cb2cefb6ab67337e79d3b93cb2916e26b
Administrator <xyf_0704@sina.com>
2019-03-07 11:35:53 +0800

    update

--- tests/qachat.py
+++ tests/qachat.py
@@ -5,7 +5,7 @@
 @file: qachat.py
 @Software: PyCharm
 @time: 2019/2/27 10:38
-@desc: 一个最简单的Q&A问答，快速实现，重复模块、结构、模型未优化
+@desc: 一个最简单的Q&A问答，快速实现，未优化重复模块、结构、模型
 
 qa_bot.csv结构：
 -------------------------------------------------------------------------------------
@@ -23,22 +23,10 @@ import jieba
 import csv
 import pandas as pd
 
-texts1 = []
-texts2 = []
-with open('/home/pywork/kf7899/datas/qa_bot.csv','r',encoding='gbk') as csvfile:
-    reader = csv.reader(csvfile)
-    texts1 = [row[0]for row in reader]
-with open('/home/pywork/kf7899/datas/qa_bot.csv','r',encoding='gbk') as csvfile:
-    reader = csv.reader(csvfile)
-    texts2 = [row[1]for row in reader]
-
 
 def stopwordslist(filepath):
     wlst = [w.strip() for w in open(filepath,'r',encoding='utf8').readlines()]
     return wlst
-
-
-stop_words = stopwordslist('../data/stop_words.txt')
 
 
 def seg_sentence(sentence,stop_words):
@@ -53,8 +41,13 @@ def seg_sentence(sentence,stop_words):
     return outstr.split(' ')
 
 
+# 0、获取数据
+data = pd.read_csv('/home/pywork/kf7899/datas/qa_bot.csv', encoding='gbk')
+Question = data["Question"].tolist()
+Answer = data["Answer"].tolist()
+stop_words = stopwordslist('/home/pywork/kf7899/rasa_chatbot_cn/data/stop_words.txt')
 # 1、将【文本集】生成【分词列表】
-texts = [seg_sentence(text, stop_words) for text in texts1]
+texts = [seg_sentence(text, stop_words) for text in Question]
 # 2、基于文本集建立【词典】，并提取词典特征数
 dictionary1 = corpora.Dictionary(texts)
 feature_cnt = len(dictionary1.token2id)
@@ -65,12 +58,7 @@ tfidf = models.TfidfModel(corpus)
 
 
 def get_qa(query):
-    # print("********************************************************************************")
     keyword = query
-    # keyword = input("**用户**：")
-    # if keyword == 'quit' or keyword == 'exit':
-    #     break
-
     # 5、同理，用【词典】把【搜索词】也转换为【稀疏向量】
     kw_vector = dictionary1.doc2bow(jieba.lcut(keyword))
     # 6、对【稀疏向量集】建立【索引】
@@ -81,18 +69,16 @@ def get_qa(query):
     for i in range(len(sim)):
         temp = []
         # 只取相似度大于0.6的
-        if sim[i] >= 0.6:
+        if sim[i] >= 0.8:
             temp.append(sim[i])
-            temp.append(texts1[i])
-            temp.append(texts2[i])
+            temp.append(Question[i])
+            temp.append(Answer[i])
             result.append(temp)
     df = pd.DataFrame(result, columns=['相似度分数', '问题','答案'])
     df1 = df.sort_values('相似度分数', ascending=False)
     # print(df1)
     if (len(df1)) >= 1:
-        # print("相似度分数:",df1.loc[0]['相似度分数'],"相似问题:",df1.loc[0]['问题'],"答案:",df1.loc[0]['答案'])
-        # print("--Score--:", df1.loc[0]['相似度分数'])
-        # print("**小智**:", df1.loc[0]['答案'])
         return df1.loc[0]['答案']
+        # print(df1.loc[0]['答案'],df1.loc[0]['相似度分数'])
     else:
         return "未找到答案"
